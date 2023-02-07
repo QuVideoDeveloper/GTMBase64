dd28525abdaae345f3af709de97a2b43f2c3051c
bolun.chen <bolun.chen@quvideo.com>
2023-02-07 10:48:02 +0800

    支持spm

---
+++ Sources/GTMBase64/GTMBase64.swift
@@ -0,0 +1,6 @@
+public struct GTMBase64 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/GTMBase64Tests/GTMBase64Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import GTMBase64
+
+final class GTMBase64Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+//        XCTAssertEqual(GTMBase64().text, "Hello, World!")
+    }
+}
